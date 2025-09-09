76fd793331088a7980c65f50a1704958b0a3cfae
sam21733 <samcomputerucd@gmail.com>
2025-09-09 21:27:54 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-'''from PyPDF2 import PdfReader'''
+from PyPDF2 import PdfReader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.vectorstores import FAISS
 from langchain.embeddings import HuggingFaceEmbeddings
